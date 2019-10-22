3f901887aae73ef25de8937af541d7ac117c10d5
burbage stacey <rstacey06@gmail.com>
2019-10-22 22:08:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,9 +27,6 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if shipping_manifest["top hat"]
-  shipping_manifest["top hat"] += 1
-else
-  shipping_manifest["top hat"] = 1
-end
+   hash[key] ? hash[key] += 1 :hash[key] = 1
+  hash
 end
