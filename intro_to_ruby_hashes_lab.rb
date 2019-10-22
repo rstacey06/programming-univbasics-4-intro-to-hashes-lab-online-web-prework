ff817fb8caf53cb2bb3ed78eb63f751a66df3e69
burbage stacey <rstacey06@gmail.com>
2019-10-22 21:53:36 +0000

    Automatically backed up by Learn

---
+++ intro_to_ruby_hashes_lab.rb
@@ -0,0 +1,36 @@
+def new_hash
+  {}
+
+end
+
+def my_hash
+   {
+  "one key" => "one value",
+  "two key" => "two value"
+}
+end
+
+def pioneer
+  {:name=>"Grace Hopper"}
+end
+
+def id_generator
+  id_generator = {:id => 10}
+end
+
+def my_hash_creator(key, value)
+    {
+  "one key" => "one value",
+  "two key" => "two value"
+}
+end
+
+def read_from_hash(hash, key)
+  # return the correct value using the hash and key parameters
+end
+
+def update_counting_hash(hash, key)
+  # given a hash an a key as parameters, return an updated hash
+  # if the provided key is not present in the hash, add it and assign it to the value of 1
+  # if the provided key is present, increment its value by 1
+end
